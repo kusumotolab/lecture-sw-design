e772eb856c5a873db7d491ed3d28f76dff415eb4
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2024-07-22 08:09:17 +0000

    deploy: 83803e9113f00217bccb0ea37ae613ecdbfa3390

--- src/semver.py
+++ src/semver.py
@@ -1,6 +1,5 @@
-
-def isSemVer(ver):
+def isSemVer(v: str) -> bool:
     import re
     numeric = '(0|[1-9][0-9]*)'
     pattern = r'^%s.%s.%s$' % (numeric, numeric, numeric)
-    return re.match(pattern, ver) != None
+    return re.match(pattern, v) != None
